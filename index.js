e0b54a324206a9867646f35616eab0c651b45ef6
StevenMenzel <stevenmenzel@hotmail.de>
2022-05-12 22:17:18 +0200

    public folder and partials test

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 import express from 'express';
 import {engine} from'express-handlebars';
+import path from 'path';
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const server = express();
 //Middlewares
 ////Handlebars
@@ -8,6 +12,7 @@ server.engine('handlebars', engine({
 }));
 server.set('view engine','handlebars');
 server.set('views', './views')
+server.use(express.static(path.join(__dirname, 'public')));
 //Routes
 /// Home
 server.get('/', (req , res)=>{
